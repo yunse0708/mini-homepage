0d1b73a074c31e4784cf55842040f929a7e436f2
yunse0708 <Yunse070708@gmail.com>
2023-07-13 09:32:14 +0900

    게임 초기화 생성

--- js/game.js
+++ js/game.js
@@ -1,55 +1,55 @@
 document.getElementById('startButton').addEventListener('click', startGame);
+document.getElementById('resetButton').addEventListener('click', resetGame);
 
-function startGame() {
-  // 게임 시작 버튼을 숨깁니다.
-  document.getElementById('startButton').style.display = 'none';
-  // 게임 화면을 보이도록 설정합니다.
-  document.getElementById('gamePage').classList.remove('hidden');
-}
-
-const cards = document.querySelectorAll('.memory-card');
-
+let cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+function startGame() {
+  document.getElementById('startButton').style.display = 'none';
+  document.getElementById('gamePage').classList.remove('hidden');
+}
+
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
-
+  
   this.classList.add('flip');
-
+  
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
-
     return;
   }
-
+  
   secondCard = this;
   checkForMatch();
 }
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
-  isMatch ? disableCards() : unflipCards();
+  
+  if (isMatch) {
+    disableCards();
+    checkWinCondition();
+  } else {
+    unflipCards();
+  }
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-
   resetBoard();
 }
 
 function unflipCards() {
   lockBoard = true;
-
+  
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-
     resetBoard();
   }, 1500);
 }
@@ -57,6 +57,39 @@ function unflipCards() {
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
+}
+
+function checkWinCondition() {
+  const flippedCards = document.querySelectorAll('.flip');
+  if (flippedCards.length === cards.length) {
+    showResetButton();
+  }
+}
+
+function resetGame() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+    card.classList.remove('flip');
+    card.addEventListener('click', flipCard);
+  });
+
+  resetBoard();
+  hideResetButton();
+  showStartButton();
+  hideGamePage();
+}
+
+function showResetButton() {
+  document.getElementById('resetButton').classList.remove('hidden');
+}
+
+function hideResetButton() {
+  document.getElementById('resetButton').classList.add('hidden');
+}
+
+function hideGamePage() {
+  document.getElementById('gamePage').classList.add('hidden');
 }
 
 (function shuffle() {
